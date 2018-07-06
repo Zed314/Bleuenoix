46e6d9df1194f84680a2c8ad2a92ab0e633fed79
Ziggy Vergne <ziggy.vergne@gmail.com>
2018-07-06 23:52:59 +0200

    Add vote overlay and overlay in the modal

--- bleuenoix/static/bleuenoix/script.js
+++ bleuenoix/static/bleuenoix/script.js
@@ -7,12 +7,15 @@ $(document).ready(function () {
       url: "/memes/getAllMemes",
       data: {},
       success: function (data) {
-
-        console.log(data.memes);
         var memes = data.memes;
         $.each(memes, function (index, value) {
-          var divWrapper = $("<div>");
-          var memeToAdd = $("<a>", {
+          var divWrapper = $("<div class ='container-overlay'>");
+          var memeToAdd = $("<div>");
+          memeToAdd.append($("<div>", {
+            "class": "center-cropped",
+            "style": "background-image: url(" + value.image + ");"
+          }));
+          var aboveoverlay = $("<div>", {
             "data-meme-id": value.id,
             "data-meme-uploader": value.uploader,
             "data-meme-upvotes": value.upvoters,
@@ -22,56 +25,73 @@ $(document).ready(function () {
             "data-meme-url": value.image,
             "data-meme-editable": value.editable,
             "data-meme-deletable": value.deletable,
-            "class": "modal-opener",
+            "class": "modal-opener above-overlay",
             "data-toggle": "modal",
             "data-target": "#memeModal"
           });
-          memeToAdd.append($("<div>", {
-            "class": "center-cropped",
-            "style": "background-image: url(" + value.image + ");"
-          }));
+          var overlay = $("<div>", {
+            "class": "overlay"
+          });
+          var buttonUpVote = $("<span>", {
+            "class": "vote-button no-modal",
+            "data-meme-id": value.id,
+            "data-appreciation": "like"
+          });
+          buttonUpVote.html("👍");
+          var buttonDownVote = $("<span>", {
+            "class": "vote-button no-modal",
+            "data-meme-id": value.id,
+            "data-appreciation": "dislike"
+          });
+          buttonDownVote.html("👎");
+          overlay.append(buttonUpVote);
+          overlay.append(buttonDownVote);
+          memeToAdd.append(aboveoverlay);
+          memeToAdd.append(overlay);
           divWrapper.append(memeToAdd);
           $("#grid").append(divWrapper);
-          salvattore.rescanMediaQueries();
-         
+
+          buttonUpVote.click({ memeid: value.id }, votefunction);
+
+          buttonDownVote.click({ memeid: value.id }, votefunction);
+
         });
+        salvattore.rescanMediaQueries();
       }
     })
 
-    $('#memeModal').on('show.bs.modal', function (e) {
+  $('#memeModal').on('show.bs.modal', function (e) {
+    //get data-id attribute of the clicked element
+    $("#modal-title").text($(e.relatedTarget).data('meme-title'));
+    //populate the textboxe.currentTarget).find('#inside').val($(e.relatedTarget).data('meme-id'));
+    $("#img-modal").attr("src", $(e.relatedTarget).data('meme-url'));
+    $(".vote-button").attr("data-meme-id", $(e.relatedTarget).data('meme-id'));
+    $('#number-upvotes').text($(e.relatedTarget).data('meme-upvotes'));
+    $('#number-downvotes').text($(e.relatedTarget).data('meme-downvotes'));
+    $('#meme-uploader').text($(e.relatedTarget).data('meme-uploader'));
+    $('#meme-category').text($(e.relatedTarget).data('meme-category'));
+    $('#edit-button').attr("href", "updatememe/" + $(e.relatedTarget).data('meme-id'));
+    $('#delete-button').attr("href", "deletememe/" + $(e.relatedTarget).data('meme-id'));
 
-      //get data-id attribute of the clicked element
-      $("#modal-title").text($(e.relatedTarget).data('meme-title'));
-      //populate the textboxe.currentTarget).find('#inside').val($(e.relatedTarget).data('meme-id'));
-      $("#img-modal").attr("src", $(e.relatedTarget).data('meme-url'));
-      $(".vote-button").attr("data-meme-id", $(e.relatedTarget).data('meme-id'));
-      $('#number-upvotes').text($(e.relatedTarget).data('meme-upvotes'));
-      console.log($(e.relatedTarget).data());
-      $('#number-downvotes').text($(e.relatedTarget).data('meme-downvotes'));
-      $('#meme-uploader').text($(e.relatedTarget).data('meme-uploader'));
-      $('#meme-category').text($(e.relatedTarget).data('meme-category'));
-      $('#edit-button').attr("href", "updatememe/" + $(e.relatedTarget).data('meme-id'));
-      $('#delete-button').attr("href", "deletememe/" + $(e.relatedTarget).data('meme-id'));
+    if ($(e.relatedTarget).data('meme-editable') === true) {
+      $('#edit-button').show();
+    }
+    else {
+      $('#edit-button').hide();
 
-      if ($(e.relatedTarget).data('meme-editable') === true) {
-        $('#edit-button').show();
-      }
-      else {
-        $('#edit-button').hide();
-       
-      }
-      if ($(e.relatedTarget).data('meme-deletable') === true) {
-        $('#delete-button').show();
-  
-      }
-      else {
-        $('#delete-button').hide();
-      
-      }
-      $('.vote-button').unbind();
-      $(".vote-button").click({ memeid: $(e.relatedTarget).data('meme-id') }, votefunction);
-      $('#delete-button').click({ memeid: $(e.relatedTarget).data('meme-id') }, deletefunction);
-    });
+    }
+    if ($(e.relatedTarget).data('meme-deletable') === true) {
+      $('#delete-button').show();
+
+    }
+    else {
+      $('#delete-button').hide();
+
+    }
+    $('.vote-button').unbind();
+    $(".vote-button").click({ memeid: $(e.relatedTarget).data('meme-id') }, votefunction);
+    $('#delete-button').click({ memeid: $(e.relatedTarget).data('meme-id') }, deletefunction);
+  });
   function deletefunction(event) {
     var memeid = event.data.memeid;
     $.ajax(
