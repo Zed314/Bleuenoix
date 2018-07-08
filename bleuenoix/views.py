c0f31f9e7c465bcf6e2263b1cdb74bd1471ce993
Ziggy Vergne <ziggy.vergne@gmail.com>
2018-07-09 01:02:11 +0200

    Removed unused view

--- bleuenoix/views.py
+++ bleuenoix/views.py
@@ -176,22 +176,3 @@ class SeeMeme(DetailView):
     model = Meme
     context_object_name = "meme"
     template_name = "bleuenoix/explore.html"
-
-
-def sendmemes(request):
-    # Construire le formulaire, soit avec les données postées,
-    # soit vide si l'utilisateur accède pour la première fois
-    # à la page.
-    form = MemeForm(request.POST or None, request.FILES)
-    # Nous vérifions que les données envoyées sont valides
-    # Cette méthode renvoie False s'il n'y a pas de données
-    # dans le formulaire ou qu'il contient des erreurs.
-    if form.is_valid():
-        # Ici nous pouvons traiter les données du formulaire
-        title = form.cleaned_data['title']
-        auteur = form.cleaned_data['auteur']
-        image = form.cleaned_data["image"]
-        Meme(title=title, auteur=auteur, image=image).save()
-        form.save()
-    # Quoiqu'il arrive, on affiche la page du formulaire.
-    return render(request, 'bleuenoix/sendmemes.html', locals())
